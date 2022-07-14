579d5642f0d7bce038ba926af4f2b7701d484d9d
Mansi Saxena <77928648+Mansi1403@users.noreply.github.com>
2022-07-14 12:53:30 +0530

    Create ClientApp.java

---
+++ MavenDemoGit/src/main/java/com/lti/entity/ClientApp.java
@@ -0,0 +1,13 @@
+package com.lti.entity;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c=new Calculator();
+		
+		int r=c.add(100,200);
+		System.out.println(r);
+	}
+
+}
